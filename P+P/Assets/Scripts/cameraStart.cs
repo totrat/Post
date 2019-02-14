af476a4cb66069d8ba5d0df0c1a84282d6528c67
totrat <higashikatatoshiki@gmail.com>
2019-02-14 10:36:21 +0900

    カメラの試作品

---
+++ P+P/Assets/Scripts/cameraStart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraStart : MonoBehaviour { 
+	// Use this for initialization
+	void Start () {
+        Vector3 bike = GameObject.Find("bike").transform.position;
+        GameObject.Find("fmcamera").transform.position = new Vector3(bike.x, bike.y, bike.z);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
