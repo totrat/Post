dd4631477ed9c2ad417f63608bd39c444f5a9dda
totrat <higashikatatoshiki@gmail.com>
2019-02-15 13:09:58 +0900

    okGoogle! Can you do it?

---
+++ P+P/Assets/Scripts/fpPlayercamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fpPlayercamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 bike = GameObject.Find("bike").transform.position;
+        GameObject.Find("fmcamera").transform.position = new Vector3(bike.x-20, bike.y + 30, bike.z);
+        bike = GameObject.Find("bike").transform.localEulerAngles;
+        GameObject.Find("fmcamera").transform.localEulerAngles = new Vector3(bike.x, bike.y, bike.z);
+    }
+}
---
+++ P+P/Assets/Scripts/score.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour {
+    static public int denemy=0;//消えた敵数
+    private Text Scoretext;//スコア用の文字
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Scoretext.text = denemy.ToString("000") + "/100" ;//スコアの表示
+    }
+}
